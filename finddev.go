44a2506f027e50c7c495159450f86f77cafd6e2d
edwin.twest@gmail.com <edwin.twest@gmail.com>
2018-10-08 22:56:17 -0700

    added interface info gathering.

--- finddev.go
+++ finddev.go
@@ -1,12 +1,20 @@
 package main
 
 import (
-  "fmt"
-  //"github.com/google/gopacket"
-  "github.com/google/gopacket/pcap"
+	"fmt"
+	//"github.com/google/gopacket"
+	"github.com/google/gopacket/pcap"
 )
 
+func getinfo(i pcap.Interface) {
+	for j := 0; j < len(i.Addresses); j++ {
+		fmt.Println(i.Name, i.Addresses[j].IP)
+	}
+}
+
 func main() {
-    value, _ := pcap.FindAllDevs()
-    fmt.Println(value)
+	value, _ := pcap.FindAllDevs()
+	for i := 0; i < len(value); i++ {
+		getinfo(value[i])
+	}
 }
